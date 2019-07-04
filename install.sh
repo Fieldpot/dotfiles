0d0a975f7b41ca4132744f28682130950658c3f1
Kazuhisa Tanabe <fieldpotsec@gmail.com>
2019-07-05 07:56:04 +0900

    Fix symbolic link

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ ln -s $dir ~/.config/nvim
 
 # install zplug
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
-ln -s ~/dotfiles/zsh/.zshrc ~/
+ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
 
 # install .pyenv
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
